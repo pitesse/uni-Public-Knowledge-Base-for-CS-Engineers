9cb4fa7d70efc480842a053dc23c31cffc3c588c
pitesse <pietro.pizzoccheri@mail.polimi.it>
2025-11-24 18:31:19 +0100

    Add implementations for Functors, Applicatives, Monads, and Writer Monads in Haskell

---
+++ PPL/haskell/Applicatives.hs
@@ -0,0 +1,84 @@
+module Applicatives where
+
+import Functors (Result)
+
+-- if we do:
+-- :t (+) <$> Ok 3
+-- we get a function and a value both wrapped into Ok (Result), but with no way to apply
+-- one to the other.
+
+-- For now, we don't really have an easy way to sum the values inside two Okes
+-- (<$> Ok 3) <$> ((+) <$> Ok 2)
+-- even something like this doesnt work, because we have a Ok wrapped in a Ok
+
+-- we'd like something with a signature like
+-- Ok (a -> b) -> Ok a -> Ok b
+-- to apply functions in the context (the Ok) of a functor
+
+-- we'd like to write
+-- (+) <$> Just 2 <*> Just 3
+
+-- ...AND IT WORKS! That's because
+-- (<*>) :: Applicative f => f (a -> b) -> f a -> f b
+
+-- Applicative functors
+-- ...continuing with the 'container' example
+-- Requires:
+-- 1. a method 'pure' that takes a value, and returns a Functor containing such value.
+-- 2. a method '<*>', that works like 'fmap', but
+--    instead of taking a function, it receives a
+--    Functor containing a function, and applies it
+--    to a container of the same type
+-- 3. the type constructor must be a functor, too
+
+-- For list, <*> is concatMap, which first maps a
+-- function to each list, and then concats the results.
+
+{--
+    instance Applicative [] where
+    pure x = [x]
+    fs <*> xs = concatMap (\f -> map f xs) fs
+--}
+-- example:
+-- ghci> [(+1), (*2), (+10)] <*> [1,2,3]
+-- [2, 3, 4, 2, 4, 6, 11, 12, 13]
+
+-- WHY?
+-- 'fmap' limits us to apply FUNCTIONS to a
+-- 'container'. What if the function(s) we want to
+-- apply are inside another 'container'? We can't do
+-- that with functors. Hence, applicative to the rescue.
+-- FUN-IN-CONT <*> VALUES-IN-CONT
+
+-- example: pure (+) <*> [5] <*> [3]
+-- Q: what does 'pure (+) <*> [5] returns?
+-- A: a list of partially applied functions
+-- same for [(+), (*)] <*> [5]
+
+-- since `pure f <*> x` equals `fmap f x`
+-- we can write `pure f <*> x <*> y <*> ...`
+-- as `fmap f x <*> y <*> ...` or even better
+-- `f <$> x <*> y <*> ...` (<$> is infix of 'fmap')
+
+{--
+APPLICATIVE RULES:
+pure id <*> v = v                            -- Identity
+pure f <*> pure x = pure (f x)               -- Homomorphism
+u <*> pure y = pure ($ y) <*> u              -- Interchange
+pure (.) <*> u <*> v <*> w = u <*> (v <*> w) -- Composition
+--}
+
+{--
+
+Applicative is mostly used to apply NON-UNARY functions
+to values wrapped in a context.
+
+(+) <$> Just 3 <*> Just 2 --> Just 5
+pure (+) <*> Just 3 <*> Just 2 --> Just 5
+
+This is also called lifting, and we have an alias
+for that with liftA2:
+
+liftA2 (+) (Just 3) (Just 2) --> Just 5
+
+--}
---
+++ PPL/haskell/Monads.hs
@@ -0,0 +1,289 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use <&>" #-}
+module Monads where
+
+import Data.Time (UTCTime (utctDay), getCurrentTime)
+import Data.Time.Calendar
+import Text.Read (readMaybe)
+
+{-- Let's test what we've learned by
+solving this issue:
+- we define a data type to represent numbers `Val`
+- we define a data type to represent divisions `Div`
+- `eval` is a function that evaluates a
+  math expression defined with Val and Div
+ --}
+
+data Expr
+  = Val Int
+  | Div Expr Expr
+  deriving (Eq, Show)
+
+eval :: Expr -> Int
+eval (Val n) = n
+eval (Div x y) = eval x `div` eval y
+
+-- What if we divide by ZERO?
+-- Spoiler: we crash! => Exception: divide by zero
+
+-- How can we make division by 0 it SAFE?
+-- Let's define safediv which CAN'T fail!
+
+-- first, let's define a simple "box" to hold
+-- our value if it's ok, or an 'error' if it's
+-- the result of something invalid
+data Result a = Err | Ok a
+  deriving (Eq, Ord, Show)
+
+safediv :: Int -> Int -> Result Int
+safediv n m =
+  if m == 0
+    then Err
+    else Ok (n `div` m)
+
+eval' :: Expr -> Result Int
+eval' (Val n) = Ok n
+eval' (Div x y) = case eval' x of
+  Err -> Err
+  Ok n -> case eval' y of
+    Err -> Err
+    Ok m -> safediv n m
+
+-- too verbose: we're repeating the same structure...
+-- how can we improve it?
+
+-- let's define a 'bind' function that:
+-- - takes a Result 'm' and
+-- - takes a function 'f' that take
+--   the value inside the Result m
+--   and returns a new Result
+-- - returns the result of 'f' if the value of 'm'
+--   is not Err, otherwise returns Err.
+bind :: Result a -> (a -> Result b) -> Result b
+m `bind` f = case m of
+  Err -> Err
+  Ok x -> f x
+
+eval'' :: Expr -> Result Int
+eval'' (Val n) = Ok n
+-- 'return' (:: Int -> m Int) is the generic version of
+-- 'Ok' (:: Int -> Result Int), which puts 'n'
+-- into the 'Maybe' "box" in this case.
+--
+-- We can then use the 'sequence' operator >>=
+eval'' (Div x y) =
+  eval'' x
+    `bind` ( \n ->
+               eval'' y
+                 `bind` safediv n
+           )
+
+-- we've removed the 'case' and the 'Result/Ok/Err'
+-- part, but it still does not look great...
+-- what can Haskell do for us?
+
+-- The 'DO' notation (syntactic sugar)
+-- [You need a Monad for this]
+{--
+    mEval :: Expr -> Result Int
+    mEval (Val n) = Ok n
+    mEval (Div x y) = do
+    n <- mEval x
+    m <- mEval y
+    safediv n m
+--}
+
+-- Let's redefine Result as a Monad
+-- It needs to be an 'applicative' instance
+-- So it means it also needs to be a 'functor' instance
+
+instance Functor Result where
+  fmap f (Ok x) = Ok (f x)
+  fmap _ Err = Err
+
+instance Applicative Result where
+  pure = Ok
+  Ok f <*> Ok x = Ok (f x)
+  _ <*> Err = Err -- 'whatever' applied to an Err is an Err
+  Err <*> _ = Err
+
+instance Monad Result where
+  Ok x >>= f = f x
+  Err >>= _ = Err
+
+-- we don't define return because return = pure by default!
+
+mEval :: Expr -> Result Int
+mEval (Val n) = Ok n
+mEval (Div x y) = do
+  n <- mEval x
+  m <- mEval y
+  safediv n m
+
+testOk = mEval $ Div (Val 3) (Val 1)
+
+testErr = mEval $ Div (Val 3) (Val 0)
+
+-- what we've just done is redefining the Maybe monad
+-- Simply swap:
+-- Result -> Maybe, Ok -> Just and Err -> Nothing.
+
+-- MONADS HAVE RULES!
+-- 1. LEFT IDENTITY: `return x >>= f` is the same as `f x`
+-- 2. RIGHT IDENTITY: `m >>= return` is the same as `m`
+-- 3. ASSOCIATIVITY: Doing `(m >>= f) >>= g` is like doing
+--                   `m >>= (\x -> f x >>= g)`
+
+-- It bridges the values from the PURE world to the
+-- IMPURE world.
+-- Unfortunately, you can't really write real programs
+-- without side-effect. Haskell lets you relegate
+-- side-effects to monads.
+
+-- SAME IDEA, works for other EFFECTS:
+-- I/O, Mutable State, Networking, Error Handling...
+
+-- Hello World (as a monad)
+
+greet :: String -> String
+greet name = "Hello " ++ name ++ "!"
+
+main = do
+  putStrLn "Hi, what's your name?"
+  msg <- greet <$> getLine
+  putStrLn msg
+  putStrLn "How old are you?"
+  msg <- getLine
+  -- we can use let to declare local values (in the do block)
+  -- NOTE that we don't use `in`
+  let age = readMaybe msg :: Maybe Int
+  -- Getting the Date is an IO operation.
+  -- We need to handle the IO monad again!
+  currentTime <- getCurrentTime
+  let (year, _, _) = toGregorian . utctDay $ currentTime
+  case age of
+    Just a -> do
+      let birthYear = fromInteger year - a
+      putStrLn $ "You were born in " ++ show birthYear
+    _ -> putStrLn "Something went wrong!"
+
+-- just call 'main' in ghci, it will wait for user input
+
+-- WE are the side-effect. Main handles the side-effects (reading, writing to console) through the IO monad, which 'isolates' the impure portion of the code from the rest of the 'pure' code.
+
+-- Fun Fact: The IO Monad is implemented as a State Monad, where the State that get passed around is the real world!
+
+
+
+
+
+
+
+
+
+-- ANOTHER EXAMPLE OF MONADS FROM SCRATCH:
+-- (This works if you want to reimplement monads in other languages)
+square x = x ^ 2
+
+addOne x = x + 1
+
+x = addOne $ square 2
+
+-- we'd like the have a log of all operations performed:
+data NumberWithLogs = NumberWithLogs
+  { number :: Int,
+    logs :: [String]
+  }
+  deriving (Show)
+
+square1 :: Int -> NumberWithLogs
+square1 x = NumberWithLogs (x ^ 2) ["Squared " ++ show x ++ " to get " ++ show (x ^ 2)]
+
+addOne1 :: NumberWithLogs -> NumberWithLogs
+addOne1 x = NumberWithLogs (number x + 1) $ logs x ++ ["Added 1 to " ++ show (number x) ++ " to get " ++ show (number x + 1)]
+
+-- of course, we can't swap these two operations
+-- solutions?
+wrapWithLogs :: Int -> NumberWithLogs
+wrapWithLogs x = NumberWithLogs x []
+
+square2 :: NumberWithLogs -> NumberWithLogs
+square2 x = NumberWithLogs (number x ^ 2) $ logs x ++ ["Squared " ++ show (number x) ++ " to get " ++ show (number x ^ 2)]
+
+-- improvements?
+
+runWithLogs :: NumberWithLogs -> (Int -> NumberWithLogs) -> NumberWithLogs
+runWithLogs input transform =
+  let newNumberWithLogs = transform (number input)
+   in NumberWithLogs (number newNumberWithLogs) (logs input ++ logs newNumberWithLogs)
+
+-- (we need to reimplement addOne :: Int -> NumberWithLogs)
+addOne2 :: Int -> NumberWithLogs
+addOne2 x = NumberWithLogs (x + 1) ["Added 1 to " ++ show x ++ " to get " ++ show (x + 1)]
+
+a = wrapWithLogs 2
+
+b = runWithLogs a square1
+
+c = b `runWithLogs` addOne2
+
+
+-- What is `wrapWithLogs` in a monad context? (return)
+-- What is `runWithLogs` in a monad context? (bind)
+-- Are Monad Laws valid? (yes)
+
+-- At this point, we can try and reimplement `runWithLogs` as a monad
+-- Let's implement functor and applicative first
+
+data WithLogs a = WithLogs
+  { val :: a,
+    log :: [String]
+  }
+  deriving (Show)
+
+instance Functor WithLogs where
+  -- fmap :: (a -> b) -> WithLogs a -> WithLogs b
+  fmap f (WithLogs val log) = WithLogs (f val) log
+
+instance Applicative WithLogs where
+  pure :: a -> WithLogs a
+  pure v = WithLogs v []
+
+  -- (<*>) :: WithLogs (a -> b) -> WithLogs a -> WithLogs b
+  (WithLogs f logf) <*> (WithLogs v logv) = WithLogs (f v) (logf ++ logv)
+
+instance Monad WithLogs where
+  -- (>>=) :: WithLogs a -> (a -> WithLogs b) -> WithLogs b
+  (WithLogs v logv) >>= f =
+    let (WithLogs v' logv') = f v
+     in WithLogs v' (logv ++ logv')
+
+addLog :: [String] -> WithLogs a -> WithLogs a
+addLog l (WithLogs v logv) = WithLogs v (logv ++ l)
+
+addOneM :: (Num a) => a -> WithLogs a
+addOneM x = do
+  addLog ["Added 1"] $ return $ x + 1
+
+multByTwoM :: (Num a) => a -> WithLogs a
+multByTwoM x = do
+  addLog ["Mult by 2"] $ return $ x * 2
+
+withLogs :: a -> WithLogs a
+withLogs = pure
+
+l = do
+  l1 <- withLogs 5
+  l2 <- addOneM l1
+  multByTwoM l2
+
+l' =
+  withLogs 5
+    >>= ( \l1 ->
+            addOneM l1
+              >>= ( \l2 ->
+                      multByTwoM l2
+                        >>= (\l3 -> return l3)
+                  )
+        )
